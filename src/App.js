ac54646db403c0564e6b064bb6b2c5af236d00bd
Spaceboy404 <isaacrich404@gmail.com>
2025-08-29 00:46:29 -0300

    new files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ThermweaveLanding from "C:/Users/isaac/Documents/Thermweave/Website/thermweave-site/src/thermweave_landing_page_react";
+import ThermweaveLanding from "./thermweave_landing_page_react";
 
 export default function App() {
   return <ThermweaveLanding />;
